a347fc5a7f0bdf4a0e15fed93d35883ba07c64d7
christineputri <christineputri32@gmail.com>
2023-12-02 15:20:04 +0700

    Initial Commit

---
+++ project12/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  project12
+//
+//  Created by prk on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func test<#Name#>() {
+        <#statements#>
+    }
+
+}
+
